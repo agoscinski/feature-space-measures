aeb7cbfbbf60bedcd50c3f1d1567fe52eb218a14
Alexander Jan Goscinski <alexander.goscinski@epfl.ch>
2020-06-03 19:12:35 +0200

    format files, add docs

--- plot_degenerated_manifold.py
+++ plot_degenerated_manifold.py
@@ -1,18 +1,18 @@
+import sys
+sys.path.insert(0, '/local/scratch/goscinsk/lib/librascal/build')
+from rascal.representations import SphericalInvariants
 import numpy as np
 import scipy
 import ase.io
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
-import sys
-sys.path.insert(0,'/local/scratch/goscinsk/lib/librascal/build')
-from rascal.representations import SphericalInvariants
 
 # Reconstructs the manifold as in Fig. 2 a) https://arxiv.org/abs/2001.11696
 
 # XYZ of manifold, plus corresponds to the C^+ manifold and minus to the C^- manifold
-frames_minus = ase.io.read('data/manif-minus.extxyz',':')
-frames_plus = ase.io.read('data/manif-plus.extxyz',':')
+frames_minus = ase.io.read('data/manif-minus.extxyz', ':')
+frames_plus = ase.io.read('data/manif-plus.extxyz', ':')
 frames = frames_minus + frames_plus
 for frame in frames:
     frame.cell = np.eye(3)*5
@@ -35,7 +35,7 @@ hypers_ps_gto = {
     'cutoff_smooth_width': 0.01,
     'normalize': normalize
 }
-hypers_bs_gto= {
+hypers_bs_gto = {
     'soap_type': 'BiSpectrum',
     'radial_basis': 'GTO',
     'interaction_cutoff': cutoff,
@@ -48,34 +48,49 @@ hypers_bs_gto= {
 }
 
 
-print('Computing representation, should take around 1 minute')
+print('Computing representations, should take around 1 minute')
 # H-H species are only considered around the central C atom environment
 representation = SphericalInvariants(**hypers_ps_gto)
 print('Start computing power spectrum...')
-soap_ps_gto_features = representation.transform(frames).get_features_by_species(representation)[(1,1)][::5]
+soap_ps_gto_features = representation.transform(
+    frames).get_features_by_species(representation)[(1, 1)][::5]
 print('Finished computing power spectrum')
 # H-H-H species are only considered around the central C atom environment
 representation = SphericalInvariants(**hypers_bs_gto)
 print('Start computing bispectrum...')
-soap_bs_gto_features = representation.transform(frames).get_features_by_species(representation)[(1,1,1)][::5]
+soap_bs_gto_features = representation.transform(
+    frames).get_features_by_species(representation)[(1, 1, 1)][::5]
 print('Finished computing bispectrum')
 
 # Extract first 3 PCA components
+
+
 def extract_principal_compononents(features):
     cov = features.T.dot(features)
-    D,U = scipy.linalg.eigh(cov)
-    return features.dot(U[:,-3:])
+    D, U = scipy.linalg.eigh(cov)
+    return features.dot(U[:, -3:])
+
+
 def plot_first_3_features(features, name):
-    X, Y, Z = features[:,0], features[:,1], features[:,2]
+    X, Y, Z = features[:, 0], features[:, 1], features[:, 2]
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X[:80]*500,Y[:80]*500,Z[:80]*500)
-    ax.scatter(X[80:]*500,Y[80:]*500,Z[80:]*500)
+    ax.scatter(X[:80]*500, Y[:80]*500, Z[:80]*500)
+    ax.scatter(X[80:]*500, Y[80:]*500, Z[80:]*500)
     plt.title(name)
     plt.show()
     plt.close()
 
-truncated_soap_bs_gto_features = extract_principal_compononents(soap_bs_gto_features)
-truncated_soap_ps_gto_features = extract_principal_compononents(soap_ps_gto_features)
-plot_first_3_features(truncated_soap_ps_gto_features, 'Power spectrum first 3 principal components')
-plot_first_3_features(truncated_soap_bs_gto_features, 'Bispectrum first 3 principal components')
+
+print('Starting eigendecomposition of power spectrum features...')
+truncated_soap_bs_gto_features = extract_principal_compononents(
+    soap_bs_gto_features)
+print('Finished eigendecomposition of power spectrum features')
+print('Starting eigendecomposition of bispectrum features...')
+truncated_soap_ps_gto_features = extract_principal_compononents(
+    soap_ps_gto_features)
+print('Finished eigendecomposition of bispectrum features')
+plot_first_3_features(truncated_soap_ps_gto_features,
+                      'Power spectrum first 3 principal components')
+plot_first_3_features(truncated_soap_bs_gto_features,
+                      'Bispectrum first 3 principal components')
--- soap_on_degenerated_manifold_example.py
+++ soap_on_degenerated_manifold_example.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.insert(0, '/local/scratch/goscinsk/lib/librascal/build')
+from feature_space_measures import reconstruction_measure_matrix
+from rascal.representations import SphericalInvariants
 import numpy as np
 import scipy
 
@@ -5,16 +9,12 @@ import ase.io
 
 import matplotlib.pyplot as plt
 from matplotlib import rc
-rc('text',usetex=True)
+rc('text', usetex=True)
 
-import sys
-sys.path.insert(0,'/local/scratch/goscinsk/lib/librascal/build')
-from rascal.representations import SphericalInvariants
 
-from feature_space_measures import reconstruction_measure_matrix
 
-frames_minus = ase.io.read('data/manif-minus.extxyz',':')
-frames_plus = ase.io.read('data/manif-plus.extxyz',':')
+frames_minus = ase.io.read('data/manif-minus.extxyz', ':')
+frames_plus = ase.io.read('data/manif-plus.extxyz', ':')
 frames = frames_minus + frames_plus
 for frame in frames:
     frame.cell = np.eye(3)*5
@@ -38,7 +38,7 @@ hypers_rs_dvr = {
     'normalize': normalize
 }
 
-hypers_rs_gto= {
+hypers_rs_gto = {
     'soap_type': 'RadialSpectrum',
     'radial_basis': 'GTO',
     'interaction_cutoff': cutoff,
@@ -62,7 +62,7 @@ hypers_ps_dvr = {
     'normalize': normalize
 }
 
-hypers_ps_gto= {
+hypers_ps_gto = {
     'soap_type': 'PowerSpectrum',
     'radial_basis': 'GTO',
     'interaction_cutoff': cutoff,
@@ -75,31 +75,41 @@ hypers_ps_gto= {
 }
 
 representation = SphericalInvariants(**hypers_rs_dvr)
-soap_rs_dvr_features = representation.transform(frames).get_features_by_species(representation)[(1,)][::5]
+soap_rs_dvr_features = representation.transform(
+    frames).get_features_by_species(representation)[(1,)][::5]
 representation = SphericalInvariants(**hypers_rs_gto)
-soap_rs_gto_features = representation.transform(frames).get_features_by_species(representation)[(1,)][::5]
+soap_rs_gto_features = representation.transform(
+    frames).get_features_by_species(representation)[(1,)][::5]
 representation = SphericalInvariants(**hypers_ps_dvr)
-soap_ps_dvr_features = representation.transform(frames).get_features_by_species(representation)[(1,1)][::5]
+soap_ps_dvr_features = representation.transform(
+    frames).get_features_by_species(representation)[(1, 1)][::5]
 representation = SphericalInvariants(**hypers_ps_gto)
-soap_ps_gto_features = representation.transform(frames).get_features_by_species(representation)[(1,1)][::5]
+soap_ps_gto_features = representation.transform(
+    frames).get_features_by_species(representation)[(1, 1)][::5]
 
-feature_spaces = [soap_rs_dvr_features, soap_rs_gto_features, soap_ps_dvr_features, soap_ps_gto_features]
+feature_spaces = [soap_rs_dvr_features, soap_rs_gto_features,
+                  soap_ps_dvr_features, soap_ps_gto_features]
 FRE_matrix, FRD_matrix = reconstruction_measure_matrix(feature_spaces)
 
+
 def plot_feature_space_measure_matrix(measure_matrix):
-    plt.figure(figsize=(8,8))
+    plt.figure(figsize=(8, 8))
     plt.imshow(measure_matrix)
-    plt.yticks(np.arange(4),[r'$\textrm{RS}_{\textrm{DVR}}$',r'$\textrm{RS}_{\textrm{GTO}}$',r'$\textrm{PS}_{\textrm{DVR}}$',r'$\textrm{PS}_{\textrm{GTO}}$'],fontsize=19)
-    plt.xticks(np.arange(4),[r'$\textrm{RS}_{\textrm{DVR}}$',r'$\textrm{RS}_{\textrm{GTO}}$',r'$\textrm{PS}_{\textrm{DVR}}$',r'$\textrm{PS}_{\textrm{GTO}}$'],fontsize=19)
-    plt.ylim(-0.5,3.5)
-    plt.title("Distortion $\|\\tilde{X}_{F'}-X_F Q\|/\|\\tilde{X}_{F'}'\|$",fontsize=22)
-    plt.xlabel("$X_{F'}$",fontsize=22)
-    plt.ylabel("$X_F$",fontsize=22)
+    plt.yticks(np.arange(4), [r'$\textrm{RS}_{\textrm{DVR}}$', r'$\textrm{RS}_{\textrm{GTO}}$',
+                              r'$\textrm{PS}_{\textrm{DVR}}$', r'$\textrm{PS}_{\textrm{GTO}}$'], fontsize=19)
+    plt.xticks(np.arange(4), [r'$\textrm{RS}_{\textrm{DVR}}$', r'$\textrm{RS}_{\textrm{GTO}}$',
+                              r'$\textrm{PS}_{\textrm{DVR}}$', r'$\textrm{PS}_{\textrm{GTO}}$'], fontsize=19)
+    plt.ylim(-0.5, 3.5)
+    plt.title(
+        "Distortion $\|\\tilde{X}_{F'}-X_F Q\|/\|\\tilde{X}_{F'}'\|$", fontsize=22)
+    plt.xlabel("$X_{F'}$", fontsize=22)
+    plt.ylabel("$X_F$", fontsize=22)
     cb = plt.colorbar()
     cb.ax.tick_params(labelsize='x-large')
     plt.tight_layout()
     plt.show()
     plt.close()
+
 
 plot_feature_space_measure_matrix(FRE_matrix)
 plot_feature_space_measure_matrix(FRD_matrix)
