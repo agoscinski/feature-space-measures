a02dbeb2c3bff7ba587afa9e64bde1186933464d
alexgo <alex.goscinski@posteo.de>
2020-06-16 22:56:59 +0200

    added kernel features implementation

--- representation.py
+++ representation.py
@@ -25,27 +25,32 @@ def compute_representation(feature_hypers, frames, evironment_idx):
     if feature_hypers["feature_type"] == "wasserstein":
         return compute_radial_spectrum_wasserstein_features(feature_hypers["feature_parameters"], frames, evironment_idx)
     else:
-        raise ValueError("The feature_type="+feature_hypres["feature_type"]+" is not known.")
+        raise ValueError("The feature_type="+feature_hypers["feature_type"]+" is not known.")
 
 def compute_hilbert_space_features(feature_hypers, frames, environment_idx):
-    return compute_features_from_kernel(compute_kernel_from_squared_distance(compute_squared_distance(feature_hypers, frames, environment_idx), feature_hypers["kernel_parameters"]))
+    return compute_features_from_kernel(compute_kernel_from_squared_distance(compute_squared_distance(feature_hypers, frames, environment_idx), feature_hypers["hilbert_space_parameters"]["kernel_parameters"]))
 
 def compute_squared_distance(feature_hypers, frames, environment_idx):
-    distance_type = feature_hypers["distance_parameters"]["distance_type"]
+    if feature_hypers["feature_type"] != "soap":
+        raise ValueError("Hilbert space features only work with soap features.")
+    print("Compute distance.")
+    distance_type = feature_hypers["hilbert_space_parameters"]["distance_parameters"]["distance_type"]
     if distance_type == "euclidean":
         representation = SphericalInvariants(**feature_hypers)
         features = representation.transform(frames).get_features(representation)[environment_idx]
         # D(A,B)**2 = K(A,A) + K(B,B) - 2*K(A,B)
         return np.sum(features ** 2, axis=1)[:, np.newaxis] + np.sum(features ** 2, axis=1)[np.newaxis, :] - 2 * features.dot(features.T)
     elif distance_type == "wasserstein":
-        return compute_wasserstein_distance(feature_hypers)
+        return compute_squared_wasserstein_distance(feature_hypers, frames, environment_idx)
     else:
         raise ValueError("The distance_type="+distance_type+" is not known.")
 
 def compute_features_from_kernel(kernel):
+    print("Compute features from kernel...")
     # SVD is more numerical stable than eigh
     U, s, _ = scipy.linalg.svd(kernel)
     # reorder eigvals and eigvectors such that largest eigvenvectors and eigvals start in the fist column
+    print("Compute features from kernel finished.")
     return np.flip(U, axis=1).dot(np.diag(np.flip(s)))
 
 # use this implementation to check for negative eigenvalues for debugging
@@ -57,7 +62,8 @@ def compute_features_from_kernel(kernel):
 #    # flip such that largest eigvals are on the left
 #    return np.flip(U, axis=1).dot(np.diag(np.sqrt(np.flip(d))))
 
-def compute_kernel(squared_distance, kernel_parameters):
+def compute_kernel_from_squared_distance(squared_distance, kernel_parameters):
+    print("Compute kernel.")
     kernel_type = kernel_parameters["kernel_type"]
     if kernel_type == "center":
         H = np.eye(len(squared_distance)) - np.ones((len(squared_distance), len(squared_distance))) / len(squared_distance)
--- wasserstein.py
+++ wasserstein.py
@@ -1,19 +1,87 @@
 from rascal.representations import SphericalInvariants
 import numpy as np
+from scipy.spatial.distance import cdist, pdist, squareform
 from scipy.interpolate import interp1d
 
 def compute_squared_wasserstein_distance(feature_paramaters, frames, environment_idx):
-    if feature_paramaters["soap_type"] == "RadialSpectrum":
-        compute_squared_radial_spectrum_wasserstein_distance(feature_paramaters, frames)
-    elif feature_paramaters["soap_type"] == "PowerSpectrum":
-        raise ValueError("The soap_type="+feature_paramaters["soap_type"]+" is not implemented yet.")
+    if feature_paramaters["feature_parameters"]["soap_type"] == "RadialSpectrum":
+        return compute_squared_radial_spectrum_wasserstein_distance(feature_paramaters, frames, environment_idx)
+    elif feature_paramaters["feature_parameters"]["soap_type"] == "PowerSpectrum":
+        raise ValueError("The soap_type="+feature_paramaters["feature_parameters"]["soap_type"]+" is not implemented yet.")
     else:
-        raise ValueError("The soap_type="+feature_paramaters["soap_type"]+" is not known.")
+        raise ValueError("The soap_type="+feature_paramaters["feature_parameters"]["soap_type"]+" is not known.")
 
 def compute_squared_radial_spectrum_wasserstein_distance(feature_paramaters, frames, environment_idx, nb_grid_points=200):
-    compute_radial_spectrum_wasserstein_features(feature_paramaters, frames, environment_idx)
-    raise ValueError("Not Implemented")
-    return np.zeros(5)
+    if  feature_paramaters["feature_parameters"]["soap_type"] != "RadialSpectrum":
+        raise ValueError('Wasserstein features can be only computed for soap_type="RadialSpectrum".')
+    if  feature_paramaters["feature_parameters"]["radial_basis"] != "DVR":
+        raise ValueError('Wasserstein features can be only computed for radial_basis="DVR".')
+
+    nb_basis_functions = feature_paramaters["feature_parameters"]["max_radial"]
+    feature_paramaters["feature_parameters"]["max_radial"] = nb_grid_points
+    normalize_wasserstein_features = feature_paramaters["feature_parameters"]["normalize"]
+    feature_paramaters["feature_parameters"]["normalize"] = False
+    cutoff = feature_paramaters["feature_parameters"]["interaction_cutoff"]
+
+    # compute soap representation for interpolation
+    representation = SphericalInvariants(**feature_paramaters["feature_parameters"])
+    densities = representation.transform(frames).get_features(representation)[environment_idx]
+    nb_envs = densities.shape[0]
+    nb_species = densities.shape[1]//nb_grid_points
+    densities = densities.reshape(nb_envs * nb_species, nb_grid_points)
+
+    # DVR uses gaussian quadrature points as basis function, we reproduce the original grid points for the interpolation
+    density_grid, density_weights = np.polynomial.legendre.leggauss(nb_grid_points)
+    density_grid = density_grid*cutoff/2 + cutoff/2
+    density_grid = np.hstack((0, density_grid))
+    densities /= np.sqrt(density_weights)
+    cdf = np.cumsum(densities, axis=1)
+
+    # gaussian quadrature points as grid
+    if feature_paramaters["hilbert_space_parameters"]["distance_parameters"]["grid_type"] == "gaussian_quadrature":
+        interp_grid, interp_weights = np.polynomial.legendre.leggauss(nb_basis_functions)
+        interp_grid = interp_grid/2 + 0.5
+    elif feature_paramaters["hilbert_space_parameters"]["distance_parameters"]["grid_type"] == "equispaced":
+        interp_grid = np.linspace(0, 1, nb_basis_functions)
+    else:
+        raise ValueError("The wasserstein grid_type="+feature_parameters["distance_parameters"]["grid_type"] +" is not known.")
+
+    # normalize nonzero environments
+    nonzero_mask = cdf[:,-1] != 0
+    # insert the zero probabilty point at the beginning to help interpolating at the beginning
+    cdf = np.concatenate((np.zeros((cdf.shape[0],1)),cdf),axis=1)
+
+    dist = np.zeros((len(nb_envs), len(nb_envs)))
+    if feature_paramaters["hilbert_space_parameters"]["distance_parameters"]["delta_normalization"]:
+        cdf = cdf.reshape(nb_envs, nb_species, nb_grid_points+1)
+        for i in range(nb_envs):
+            for j in range(nb_envs):
+                for sp in range(nb_species):
+                    max_norm = max(cdf[i,sp,-1], cdf[j,sp,-1])
+                    cdf_i = np.copy(cdf[i,sp,:])
+                    cdf_j = np.copy(cdf[j,sp,:])
+                    cdf_i[-1] = max_norm
+                    cdf_j[-1] = max_norm
+                    interpolator_i = interp1d(cdf_i, density_grid, assume_sorted=True)
+                    interpolator_j = interp1d(cdf_j, density_grid, assume_sorted=True)
+                    wasserstein_features_i = interpolator_i(interp_grid)
+                    wasserstein_features_j = interpolator_j(interp_grid)
+                    dist[i,j] += np.sum((wasserstein_features_i-wasserstein_features_j)**2)
+        return dist
+    else:
+        cdf[nonzero_mask] /= cdf[:,-1][nonzero_mask][:,np.newaxis]
+        wasserstein_features = np.zeros((nb_envs*nb_species, nb_basis_functions))
+        for i in np.where(nonzero_mask)[0]: # subset of nb_envs*nb_species
+            interpolator = interp1d(cdf[i,:], density_grid, assume_sorted=True)
+            wasserstein_features[i,:] = interpolator(interp_grid)
+        if feature_paramaters["hilbert_space_parameters"]["distance_parameters"]["grid_type"] == "gaussian_quadrature":
+            wasserstein_features *= np.sqrt(interp_weights)
+
+        wasserstein_features = wasserstein_features.reshape(nb_envs, nb_species * nb_basis_functions)
+
+        if normalize_wasserstein_features:
+            wasserstein_features /= np.linalg.norm(wasserstein_features,axis=1)[:,np.newaxis]
+        return squareform(pdist(wasserstein_features))
 
 # 200 grid points is just chosen arbitrary to obtain an approximation which is accurate enough
 def compute_radial_spectrum_wasserstein_features(feature_paramaters, frames, environment_idx, nb_grid_points=200):
@@ -56,9 +124,10 @@ def compute_radial_spectrum_wasserstein_features(feature_paramaters, frames, env
     # insert the zero probabilty point at the beginning to help interpolating at the beginning
     cdf = np.concatenate((np.zeros((cdf.shape[0],1)),cdf),axis=1)
 
-    # gaussian quadratarue points as grid
+    # gaussian quadrature points as grid
     if feature_paramaters["grid_type"] == "gaussian_quadrature":
-        interp_grid = np.polynomial.legendre.leggauss(nb_basis_functions)[0]/2 + 0.5
+        interp_grid, interp_weights = np.polynomial.legendre.leggauss(nb_basis_functions)
+        interp_grid = interp_grid/2 + 0.5
     elif feature_paramaters["grid_type"] == "equispaced":
         interp_grid = np.linspace(0, 1, nb_basis_functions)
     else:
@@ -68,6 +137,9 @@ def compute_radial_spectrum_wasserstein_features(feature_paramaters, frames, env
     for i in np.where(nonzero_mask)[0]: # subset of nb_envs*nb_species
         interpolator = interp1d(cdf[i,:], density_grid, assume_sorted=True)
         wasserstein_features[i,:] = interpolator(interp_grid)
+
+    if feature_paramaters["grid_type"] == "gaussian_quadrature":
+        wasserstein_features *= np.sqrt(interp_weights)
 
     wasserstein_features = wasserstein_features.reshape(nb_envs, nb_species * nb_basis_functions)
 
