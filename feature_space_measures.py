e35f306cd3fb5d9fc9a43760a8842e1c5cdd383c
alexgo <alex.goscinski@posteo.de>
2020-07-03 15:07:04 +0200

    changed lfre back

--- feature_space_measures.py
+++ feature_space_measures.py
@@ -57,6 +57,7 @@ def feature_space_reconstruction_measures(
         )
     if n_test is None:
         n_test = features2.shape[0]
+
     # (\|X_{F'} - (X_F)P \|) / (\|X_F\|)
     FRE = np.linalg.norm(
         features1.dot(reconstruction_weights) - features2
@@ -321,14 +322,33 @@ def local_feature_reconstruction_error(nb_local_envs, features1_train, features2
     #features2_test_sq_sum = np.sum(features2_test**2, axis=1)
     #squared_dist = features2_test_sq_sum[:,np.newaxis] + features2_test_sq_sum - 2 * features2_test.dot(features2_test.T)
     squared_dist = np.sum(features1_train**2, axis=1) + np.sum(features1_test**2, axis=1)[:,np.newaxis] - 2 * features1_test.dot(features1_train.T)
-    for i in range(n_test):
-        local_env_idx = np.argsort(squared_dist[i])[:nb_local_envs]
-        local_features1 = standardize_features(features1_train[local_env_idx])
-        local_features2 = standardize_features(features2_train[local_env_idx])
 
+    for i in range(n_test):
+        #local_env_idx = np.argsort(squared_dist[i])[:nb_local_envs]
+        #local_features1 = standardize_features(features1_train[local_env_idx])
+        #local_features2 = standardize_features(features2_train[local_env_idx])
+
+        #reconstruction_weights = feature_space_reconstruction_weights(
+        #    local_features1, local_features2, regularizer
+        #)
+        #lfre_vec[i] = np.linalg.norm(local_features1.dot(reconstruction_weights)  - local_features2 ) / np.sqrt(len(local_env_idx))
+        #lfre_vec[i] = lfre_vec[i]**2/ n_test
+
+        local_env_idx = np.argsort(squared_dist[i])[:nb_local_envs]
+        local_features1_train = features1_train[local_env_idx]
+        local_features1_train_mean = np.mean(features1_train[local_env_idx], axis=0)
+        local_features2_train = features2_train[local_env_idx]
+        local_features2_train_mean = np.mean(features2_train[local_env_idx], axis=0)
+        # standardize
         reconstruction_weights = feature_space_reconstruction_weights(
-            local_features1, local_features2, regularizer
+            local_features1_train - local_features1_train_mean, local_features2_train - local_features2_train_mean
         )
+        # \|x_i' - \tilde{x}_i' \|^2 / n_test
+        lfre_vec[i] = np.linalg.norm(
+            (features1_test[i,:][np.newaxis,:] - local_features1_train_mean).dot(reconstruction_weights) + local_features2_train_mean
+            - features2_test[i,:][np.newaxis,:]
+        )**2 / np.sqrt(n_test)
+
         # \|x_i' - \tilde{x}_i' \|^2 / n_test
         # not evactly _i_tilde but: (x_F^{(i)}-\bar{x}_F)P_{FF'}^{(i)}
         #features1_i = features1_test[local_env_idx] - local_features1_train_mean
@@ -337,8 +357,6 @@ def local_feature_reconstruction_error(nb_local_envs, features1_train, features2
         #features2_i = features2_test[local_env_idx]
         #features2_i = features2_test[i,:][np.newaxis,:] - local_features2_train_mean
         #lfre_vec[i], lfrd_vec[i] = feature_space_reconstruction_measures(features1_i, features2_i, reconstruction_weights=reconstruction_weights, n_test=1)
-        lfre_vec[i] = np.linalg.norm(local_features1.dot(reconstruction_weights)  - local_features2 ) / np.sqrt(len(local_env_idx))
-        lfre_vec[i] = lfre_vec[i]**2/ n_test
         #lfrd_vec[i] = lfrd_vec[i]**2/ np.sqrt(n_test)
     return lfre_vec, lfrd_vec
 
