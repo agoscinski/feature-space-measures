22b5e10ff8fd72be53e29cc438a71eadf034f2b1
alexgo <alex.goscinski@posteo.de>
2020-07-14 18:04:53 +0200

    made reconstruction weights computation more memory friendly

--- feature_space_measures.py
+++ feature_space_measures.py
@@ -18,11 +18,20 @@ def feature_space_reconstruction_weights(features1, features2, regularizer=1e-6)
     --------
     array : weights P = argmin_{P'} | X_{F'} - (X_F)P' |
     """
+    
+
     print("Computing weights...")
-    regs = [linear_model.BayesianRidge(alpha_1=regularizer, alpha_2=regularizer, lambda_1=regularizer, lambda_2=regularizer) for i in range(features2.shape[1])]
-    [regs[i].fit(features1, features2[:,i]) for i in range(features2.shape[1])]
-    print("Computing weights finished.")
-    return np.array( [regs[i].coef_ for i in range(features2.shape[1])] ).T
+    W = np.zeros((features1.shape[1],features2.shape[1]))
+    for i in range( features2.shape[1] ):
+        reg = linear_model.BayesianRidge(alpha_1=regularizer, alpha_2=regularizer, lambda_1=regularizer, lambda_2=regularizer)
+        reg.fit(features1, features2[:,i])
+        W[i,:] = reg.coef_
+    return W
+    #regs = [linear_model.BayesianRidge(alpha_1=regularizer, alpha_2=regularizer, lambda_1=regularizer, lambda_2=regularizer) for i in range(features2.shape[1])]
+    #[regs[i].fit(features1, features2[:,i]) for i in range(features2.shape[1])]
+    #print("Computing weights finished.")
+    #return np.array( [regs[i].coef_ for i in range(features2.shape[1])] ).T
+
     #W = np.linalg.lstsq(features1, features2, rcond=regularizer)[0]
     #if np.linalg.norm(W) > 1e4:
     #    warnings.warn("Reconstruction weight matrix very large "+ str(np.linalg.norm(W)) +". Results could be misleading.", Warning)
---
+++ soap_ps_max_radial_experiments.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# coding: utf-8
+from experiment import gfr_pairwise_experiment
+import os
+os.environ["OMP_NUM_THREADS"] = "1" # export OMP_NUM_THREADS=4
+os.environ["OPENBLAS_NUM_THREADS"] = "1" # export OPENBLAS_NUM_THREADS=4 
+os.environ["MKL_NUM_THREADS"] = "1" # export MKL_NUM_THREADS=4
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1" # export VECLIB_MAXIMUM_THREADS=4
+os.environ["NUMEXPR_NUM_THREADS"] = "1" # export NUMEXPR_NUM_THREADS=4
+
+nb_samples = 10000
+two_split = True
+if two_split:
+    seed = 0x5f3759df
+    train_ratio = 0.5
+else:
+    seed = None
+noise_removal = False
+regularizer = 1e-6
+
+# Constant hyperparameters
+cutoff = 4
+sigma = 0.5
+cutoff_smooth_width = 0.5
+normalize = False
+
+### Experiment metadata
+
+for dataset_name in ["selection-10k.extxyz", "C-VII-pp-wrapped.xyz"]:
+    for radial_basis in ["GTO", "DVR"]:
+        ## Tested hyperparameters
+        max_radials_angulars = [(2,4),(4,4),(6,4),(8,4),(10,4),(12,4),(14,4),(16,4),(18,4)]
+        features_hypers1 = [{
+            "feature_type": "soap",
+            "feature_parameters": {
+                "soap_type": "PowerSpectrum",
+                "radial_basis": radial_basis,
+                "interaction_cutoff": cutoff,
+                "max_radial": max_radial,
+                "max_angular": max_angular,
+                "gaussian_sigma_constant": sigma,
+                "gaussian_sigma_type": "Constant",
+                "cutoff_smooth_width": cutoff_smooth_width,
+                "normalize": normalize
+            },
+            #"hilbert_space_parameters": {
+            #    "distance_parameters": {"distance_type": "euclidean"},
+            #    #"kernel_parameters": {"kernel_type": "center"}
+            #    "kernel_parameters": {"kernel_type": "rbf", "gamma": 1}
+            #}
+        } for max_radial, max_angular in max_radials_angulars]
+
+        max_radials_angulars = [(n+1,l) for n, l in max_radials_angulars]
+        features_hypers2 = [{
+            "feature_type": "soap",
+            "feature_parameters": {
+                "soap_type": "PowerSpectrum",
+                "radial_basis": radial_basis,
+                "interaction_cutoff": cutoff,
+                "max_radial": max_radial,
+                "max_angular": max_angular,
+                "gaussian_sigma_constant": sigma,
+                "gaussian_sigma_type": "Constant",
+                "cutoff_smooth_width": cutoff_smooth_width,
+                "normalize": normalize
+            },
+            #"hilbert_space_parameters": {
+            #    "distance_parameters": {"distance_type": "euclidean"},
+            #    #"kernel_parameters": {"kernel_type": "center"}
+            #    "kernel_parameters": {"kernel_type": "rbf", "gamma": 10}
+            #}
+        } for max_radial, max_angular in max_radials_angulars]
+
+        gfr_pairwise_experiment(dataset_name, nb_samples, features_hypers1, features_hypers2, two_split, train_ratio, seed, noise_removal, regularizer)
