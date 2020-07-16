d5d740474f70cd894b7ee674ad3c8b3139ff15ee
alexgo <alex.goscinski@posteo.de>
2020-07-17 01:58:48 +0200

    removed print

--- feature_space_measures.py
+++ feature_space_measures.py
@@ -409,7 +409,6 @@ def local_feature_reconstruction_error(nb_local_envs, features1_train, features2
         drop = len(np.where(squared_dist[i]<inner_epsilon)[0])
         keep = len(np.where(squared_dist[i]<outer_epsilon)[0])
         local_env_idx = local_env_idx[drop:drop+(max(nb_local_envs, keep-drop))]
-        print(len(local_env_idx))
 
         local_features1_train = features1_train[local_env_idx]
         local_features1_train_mean = np.mean(features1_train[local_env_idx], axis=0)
