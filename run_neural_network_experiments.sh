ac9f0054d6ca3770cbcbc015b235f474bedff4bc
alexgo <alex.goscinski@posteo.de>
2020-10-25 04:43:40 +0100

    more fixes in runscripts

--- run_neural_network_experiments.sh
+++ run_neural_network_experiments.sh
@@ -28,12 +28,12 @@ taskset --cpu-list 0,1,2,3,4,5,6,7,8 python experiment_soap_schnet_on_all_proper
 echo "\n##########################"
 echo "#  #"
 echo "##########################\n"
-taskset --cpu-list 0,1,2,3,4,5,6,7,8 python experiment_dimenet_convergence_on_U0_qm9.py --option
+taskset --cpu-list 0,1,2,3,4,5,6,7,8 python experiment_dimenet_convergence_on_U0_qm9.py 10000 --option
 
 echo "\n##########################"
 echo "#  #"
 echo "##########################\n"
-taskset --cpu-list 0,1,2,3,4,5,6,7,8 python experiment_soap_poly_kernel_dimenet_comparison_on_U0_qm9.py --option
+taskset --cpu-list 0,1,2,3,4,5,6,7,8 python experiment_soap_poly_kernel_dimenet_comparison_on_U0_qm9.py 10000 --opti5on
 
 #repeat
 echo "\n##########################"
